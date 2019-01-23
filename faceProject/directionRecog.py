dff7ece84ba699142489cc94231e8031f31c5030
aboriginal <cyw350446550@163.com>
2019-01-23 21:35:16 +0800

    init

---
+++ faceProject/directionRecog.py
@@ -0,0 +1,35 @@
+'''
+人脸朝向识别
+'''
+
+import dataHandler
+
+'''
+设计网络
+'''
+
+
+def set_model():
+    pass
+
+
+'''
+训练网络
+'''
+
+
+def train_model():
+    pass
+
+
+'''
+测试网络
+'''
+
+
+def test_model():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ util/dataHandler.py
@@ -0,0 +1,13 @@
+'''
+加载数据
+'''
+
+'''
+载入数据
+'''
+def load_data():
+    pass
+
+
+if __name__ == '__main__':
+    pass
